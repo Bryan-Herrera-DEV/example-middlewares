f7589950138d742cf7354d21e72edc1c5d093316
Bryan Herrera ~ ርᚱ1ናተᛰ ᚻህᚥተპᚱ <50712646+Bryan-Herrera-DEV@users.noreply.github.com>
2022-05-11 22:44:30 -0500

    Update teamRoutes.js

--- src/server/team/infraestructure/teamRoutes.js
+++ src/server/team/infraestructure/teamRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 const { tokenExists } = require("../../../middlewares/token.handler");
 const { createNewTeam } = require("../aplication/createNewTeam");
-
+// todas las rutas de aqui deben pasar por el middleware JWT de passport
 router.post('/createNewTeam', [tokenExists, passport.authenticate('jwt', { session: false })], createNewTeam); // localhost:3000
 
 module.exports = router;
