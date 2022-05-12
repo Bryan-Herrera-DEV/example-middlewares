11b17e3f0cf7bdc28939c9127cf4e9d7a1639eff
Bryan Herrera ~ ርᚱ1ናተᛰ ᚻህᚥተპᚱ <50712646+Bryan-Herrera-DEV@users.noreply.github.com>
2022-05-11 22:43:36 -0500

    Update userRoutes.js

--- src/server/user/infraestructure/userRoutes.js
+++ src/server/user/infraestructure/userRoutes.js
@@ -7,6 +7,6 @@ const { userRegister } = require("../aplication/userRegister");
 const { userLogin } = require('../aplication/userLogin');
 
 router.post('/register', userRegister); // localhost:3000/user/register
-router.post('/login', [passport.authenticate('local', { session: false })], userLogin); // localhost:3000/user/login
+router.post('/login', [passport.authenticate('local', { session: false })], userLogin); // localhost:3000/user/login  ~ middleware para la autenticacion
 
 module.exports = router;
